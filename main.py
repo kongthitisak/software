cf43de74df63b9a3fcec19dd653b520ce72ca680
kongthitisak <153721368+kongthitisak@users.noreply.github.com>
2024-01-17 15:46:21 +0700

    initial

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-import requests
-from bs4 import BeautifulSoup
+import streamlit as st
 
-url = "https://www.example.com"
-response = requests.get(url)
-
-if response.status_code == 200:
-    website_content = response.text
-    
-    soup = BeautifulSoup(response.text, "html.parser")
-    title_tag = soup.find("title")
-    if title_tag:
-        title_text = title_tag.text.strip()
-        print("Page title:", title_text)
-    else:
-        print("Title tag not found.")
-    
-
-else:
-    print("Request failed with status code:", response.status_code)
+st.write(" ฐิติศักดิ์ ตุ้ยจันทร์ ")
